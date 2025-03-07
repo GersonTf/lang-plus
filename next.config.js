590b5ce83b987b378303c0026de9cefbf4d925ff
gerson <tnii.gerson@gmail.com>
2025-03-07 15:38:46 +0100

    seo improvements 4

--- next.config.js
+++ next.config.js
@@ -9,7 +9,24 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: true
+  reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
