c9c517a8c0d014add07572a415824fef7eac2f7a
gerson <tnii.gerson@gmail.com>
2025-03-07 18:30:45 +0100

    seo improvements 18

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,9 @@
 import { MetadataRoute } from 'next'
 
+// Add static configuration for export
+export const dynamic = 'force-static'
+export const revalidate = false
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
