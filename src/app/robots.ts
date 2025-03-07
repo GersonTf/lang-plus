bc8342557ae7da872d23d8e5bdfb5172603fd4e9
gerson <tnii.gerson@gmail.com>
2025-03-07 18:16:08 +0100

    seo improvements 14

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,14 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/static/'],
+      },
+    ],
     sitemap: 'https://www.apluslanguages.eu/sitemap.xml',
+    host: 'https://www.apluslanguages.eu',
   }
 } 
--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,32 +1,44 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  try {
+    const currentDate = new Date().toISOString().split('T')[0]
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.apluslanguages.eu</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://www.apluslanguages.eu/#about</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.apluslanguages.eu/#classes</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <lastmod>${currentDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
 </urlset>`
 
-  return new NextResponse(xml, {
-    headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'public, max-age=3600',
-    },
-  })
+    return new NextResponse(xml, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'public, max-age=3600'
+      },
+    })
+  } catch (error) {
+    console.error('Error generating sitemap:', error)
+    return new NextResponse('Error generating sitemap', {
+      status: 500,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+  }
 } 
